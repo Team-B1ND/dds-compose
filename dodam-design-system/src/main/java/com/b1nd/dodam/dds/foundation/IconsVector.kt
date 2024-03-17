ba1ce25c6b39505bf8ec9597a9ac56c53c7a8e26
윤석규 <greeentea.dev@gmail.com>
2024-03-18 00:18:22 +0900

    feat: Add dodam icons

---
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/foundation/IconsVector.kt
@@ -0,0 +1,80 @@
+package com.b1nd.dodam.dds.foundation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.b1nd.dodam.dds.R
+
+object DodamIcons {
+    val Home
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_home)
+
+    val ForkAndKnife
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_fork_and_knife)
+
+    val DoorOpen
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_door_open)
+
+    val Calendar
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_calendar)
+
+    val MoonPlus
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_moon_plus)
+
+    val Menu
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_menu)
+
+    val XMarkCircle
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_xmark_circle)
+
+    val ExclamationMarkCircle
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_exclamationmark_circle)
+
+    val Plus
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_plus)
+
+    val Note
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_note)
+
+    val Bus
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_bus)
+
+    val Megaphone
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_megaphone)
+
+    val ArrowLeft
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_arrow_left)
+
+    val Bell
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_bell)
+
+    val Eye
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_eye)
+
+    val EyeSlash
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_eye_slash)
+
+    val Checkmark
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_checkmark)
+
+    val CheckmarkCircle
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_checkmark_circle)
+}
