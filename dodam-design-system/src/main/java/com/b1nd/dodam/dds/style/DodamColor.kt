d88b389a7d19271081b2a61f4ce18c2adefe70a0
윤석규 <greeentea.dev@gmail.com>
2024-03-18 00:19:05 +0900

    feat: Update dodam color

--- dodam-design-system/src/main/java/com/b1nd/dodam/dds/style/DodamColor.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/style/DodamColor.kt
@@ -3,39 +3,35 @@ package com.b1nd.dodam.dds.style
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
-import com.b1nd.dodam.dds.foundation.Blue
-import com.b1nd.dodam.dds.foundation.Gray200
-import com.b1nd.dodam.dds.foundation.Gray300
-import com.b1nd.dodam.dds.foundation.Gray400
-import com.b1nd.dodam.dds.foundation.Gray600
-import com.b1nd.dodam.dds.foundation.Gray700
-import com.b1nd.dodam.dds.foundation.Gray750
-import com.b1nd.dodam.dds.foundation.Gray850
-import com.b1nd.dodam.dds.foundation.Gray900
-import com.b1nd.dodam.dds.foundation.White
+import com.b1nd.dodam.dds.foundation.DodamColor
 
-internal val LightColorScheme = lightColorScheme(
-    primary = Blue,
-    onPrimary = White,
-    secondary = Gray200,
-    tertiary = Gray750,
+
+internal val DodamLightColorScheme = lightColorScheme(
+    primary = DodamColor.Blue,
+    onPrimary = DodamColor.White,
+    secondary = DodamColor.Gray200,
+    tertiary = DodamColor.Gray750,
+    error = DodamColor.Red,
+    onError = DodamColor.White,
     surface = Color(0xFFF2F5F8),
-    onSurface = Gray900,
-    surfaceContainer = White,
-    onSurfaceVariant = Gray400,
-    outline = Gray300,
-    outlineVariant = Gray200,
+    onSurface = DodamColor.Gray900,
+    surfaceContainer = DodamColor.White,
+    onSurfaceVariant = DodamColor.Gray400,
+    outline = DodamColor.Gray300,
+    outlineVariant = DodamColor.Gray200,
 )
 
-internal val DarkColorScheme = darkColorScheme(
-    primary = Blue,
-    onPrimary = White,
-    secondary = Gray750,
-    tertiary = Gray700,
-    surface = Gray900,
-    onSurface = White,
-    surfaceContainer = Gray850,
-    onSurfaceVariant = Gray600,
-    outline = Gray700,
-    outlineVariant = Gray600,
+internal val DodamDarkColorScheme = darkColorScheme(
+    primary = DodamColor.Blue,
+    onPrimary = DodamColor.White,
+    secondary = DodamColor.Gray750,
+    tertiary = DodamColor.Gray700,
+    error = DodamColor.Red,
+    onError = DodamColor.White,
+    surface = DodamColor.Gray900,
+    onSurface = DodamColor.White,
+    surfaceContainer = DodamColor.Gray850,
+    onSurfaceVariant = DodamColor.Gray600,
+    outline = DodamColor.Gray700,
+    outlineVariant = DodamColor.Gray600,
 )
