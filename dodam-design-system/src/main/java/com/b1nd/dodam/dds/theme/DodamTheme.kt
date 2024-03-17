8ccb54afd2882e824de126bb085127231bbd87c0
윤석규 <greeentea.dev@gmail.com>
2024-03-17 15:49:51 +0900

    feat: Add dodam color

---
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/theme/DodamTheme.kt
@@ -0,0 +1,42 @@
+package com.b1nd.dodam.dds.theme
+
+import android.app.Activity
+import android.os.Build
+import android.view.View
+import android.view.WindowInsetsController
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.b1nd.dodam.dds.style.DarkColorScheme
+import com.b1nd.dodam.dds.style.LightColorScheme
+
+@Composable
+fun DodamTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+            windowInsetsController.apply {
+                hide(WindowInsetsCompat.Type.systemBars())
+                systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme,
+        typography = ,
+        content = content
+    )
+}
