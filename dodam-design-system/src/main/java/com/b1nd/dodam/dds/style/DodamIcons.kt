3ea14cf4dd6e991393f5159805a42de99e23f626
윤석규 <greeentea.dev@gmail.com>
2024-03-18 00:18:22 +0900

    feat: Add dodam icons

---
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/style/DodamIcons.kt
@@ -0,0 +1,259 @@
+package com.b1nd.dodam.dds.style
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.b1nd.dodam.dds.foundation.DodamIcons
+
+@Composable
+fun HomeIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Home,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun ForkAndKnifeIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.ForkAndKnife,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun DoorOpenIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.DoorOpen,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun CalendarIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Calendar,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun MoonPlusIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.MoonPlus,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun MenuIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Menu,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun XMarkCircleIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.XMarkCircle,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun ExclamationMarkCircleIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.ExclamationMarkCircle,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun PlusIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Plus,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun NoteIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Note,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun BusIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Bus,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun MegaphoneIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Megaphone,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun ArrowLeftIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Bus,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun BellIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Bell,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun EyeIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Eye,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun EyeSlashIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.EyeSlash,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun CheckmarkCircleIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.CheckmarkCircle,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun CheckmarkIcon(
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.Unspecified,
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = DodamIcons.Checkmark,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
