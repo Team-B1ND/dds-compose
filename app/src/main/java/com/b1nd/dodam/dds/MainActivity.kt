231534ad7ab964a746ad65d817caeb35eb6f38d2
윤석규 <greeentea.dev@gmail.com>
2024-03-18 12:04:03 +0900

    feat: Create dodam top app bar

--- app/src/main/java/com/b1nd/dodam/dds/MainActivity.kt
+++ app/src/main/java/com/b1nd/dodam/dds/MainActivity.kt
@@ -3,44 +3,24 @@ package com.b1nd.dodam.dds
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.size
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.b1nd.dodam.dds.ui.theme.DodamDesignSystemTheme
+import androidx.compose.ui.unit.dp
+import com.b1nd.dodam.dds.component.DodamBadge
+import com.b1nd.dodam.dds.component.DodamIconButton
+import com.b1nd.dodam.dds.component.DodamTopAppBar
+import com.b1nd.dodam.dds.style.HomeIcon
+import com.b1nd.dodam.dds.theme.DodamTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DodamDesignSystemTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+           DodamTheme {
+               DodamIconButton(onClick = { /*TODO*/ }) {
+                   HomeIcon()
+               }
+           }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DodamDesignSystemTheme {
-        Greeting("Android")
     }
 }
