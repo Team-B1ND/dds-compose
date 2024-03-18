76d485365ec3fcb04d85e3b793215f61dce2c169
윤석규 <greeentea.dev@gmail.com>
2024-03-18 12:03:33 +0900

    feat: Add dodam shape

---
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/foundation/ShapeToken.kt
@@ -0,0 +1,12 @@
+package com.b1nd.dodam.dds.foundation
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+object DodamShape {
+    val ExtraSmall = RoundedCornerShape(4.dp)
+    val Small = RoundedCornerShape(8.dp)
+    val Medium = RoundedCornerShape(10.dp)
+    val Large = RoundedCornerShape(12.dp)
+    val ExtraLarge = RoundedCornerShape(18.dp)
+}
