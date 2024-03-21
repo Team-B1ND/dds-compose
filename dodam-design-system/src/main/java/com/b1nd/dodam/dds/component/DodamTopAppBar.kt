309c6deacf40d8b2d9222b91aee804db3474f9d1
윤석규 <greeentea.dev@gmail.com>
2024-03-21 14:43:20 +0900

    feat: Create dodam button

--- dodam-design-system/src/main/java/com/b1nd/dodam/dds/component/DodamTopAppBar.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/component/DodamTopAppBar.kt
@@ -5,13 +5,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
@@ -20,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.b1nd.dodam.dds.component.button.DodamIconButton
 import com.b1nd.dodam.dds.style.ArrowLeftIcon
 import com.b1nd.dodam.dds.style.BellIcon
 import com.b1nd.dodam.dds.style.HeadlineMedium
--- dodam-design-system/src/main/java/com/b1nd/dodam/dds/foundation/ShapeToken.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/foundation/ShapeToken.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
 
 object DodamShape {
-    val ExtraSmall = RoundedCornerShape(4.dp)
-    val Small = RoundedCornerShape(8.dp)
-    val Medium = RoundedCornerShape(10.dp)
-    val Large = RoundedCornerShape(12.dp)
-    val ExtraLarge = RoundedCornerShape(18.dp)
+    val ExtraSmall = RoundedCornerShape(8.dp)
+    val Small = RoundedCornerShape(10.dp)
+    val Medium = RoundedCornerShape(12.dp)
+    val Large = RoundedCornerShape(18.dp)
+    val ExtraLarge = RoundedCornerShape(28.dp)
 }
