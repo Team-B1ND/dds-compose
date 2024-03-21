42cb0356bf63f900b46f69ee6d68f77ad3570a12
윤석규 <greeentea.dev@gmail.com>
2024-03-21 14:43:20 +0900

    feat: Create dodam button

---
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/component/button/DodamButton.kt
@@ -0,0 +1,495 @@
+package com.b1nd.dodam.dds.component.button
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.minimumInteractiveComponentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.b1nd.dodam.dds.animation.LoadingDotsIndicator
+import com.b1nd.dodam.dds.animation.bounceClick
+import com.b1nd.dodam.dds.style.BodyLarge
+import com.b1nd.dodam.dds.style.BodyMedium
+import com.b1nd.dodam.dds.style.BodySmall
+import com.b1nd.dodam.dds.theme.DodamTheme
+
+@Composable
+fun BoxScope.DodamCTAButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = MaterialTheme.shapes.large,
+    colors: ButtonColors = ButtonDefaults.buttonColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(0.5f),
+        disabledContainerColor = MaterialTheme.colorScheme.primary.copy(0.5f)
+    ),
+    elevation: Float = 0.0f,
+    border: BorderStroke? = null,
+    backgroundPadding: PaddingValues = PaddingValues(16.dp, 24.dp, 16.dp, 8.dp),
+    backgroundColor: Color = MaterialTheme.colorScheme.background,
+    contentPadding: PaddingValues = PaddingValues(horizontal = 24.dp, vertical = 16.dp),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    text: String,
+    icon: ImageVector? = null,
+    isLoading: Boolean = false,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.BottomCenter)
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        backgroundColor.copy(alpha = 0f),
+                        backgroundColor
+                    )
+                )
+            )
+            .padding(backgroundPadding),
+    ) {
+        Box(
+            modifier = modifier
+                .minimumInteractiveComponentSize()
+                .graphicsLayer(shadowElevation = elevation, shape = shape, clip = false)
+                .then(if (border != null) Modifier.border(border, shape) else Modifier)
+                .background(
+                    color = if (!isLoading && enabled) colors.containerColor else colors.disabledContainerColor,
+                    shape = shape
+                )
+                .clip(shape)
+                .bounceClick(
+                    interactionSource = interactionSource,
+                    indication = null,
+                    interactionColor = Color.Transparent,
+                    enabled = !isLoading && enabled,
+                    onClick = onClick,
+                    role = Role.Button
+                ),
+            propagateMinConstraints = true
+        ) {
+            Row(
+                Modifier
+                    .defaultMinSize(minHeight = 58.dp)
+                    .fillMaxWidth()
+                    .padding(contentPadding)
+                    .animateContentSize(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                content = {
+                    if (isLoading) {
+                        LoadingDotsIndicator()
+                    } else {
+                        icon?.let {
+                            Icon(
+                                modifier = Modifier.size(20.dp),
+                                imageVector = it,
+                                contentDescription = null,
+                                tint = if (enabled) colors.contentColor else colors.disabledContentColor,
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                        }
+                        BodyLarge(
+                            text = text,
+                            color = if (enabled) colors.contentColor else colors.disabledContentColor,
+                        )
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun DodamCTAButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = MaterialTheme.shapes.large,
+    colors: ButtonColors = ButtonDefaults.buttonColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(0.5f),
+        disabledContainerColor = MaterialTheme.colorScheme.primary.copy(0.5f)
+    ),
+    elevation: Float = 0.0f,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = PaddingValues(horizontal = 24.dp, vertical = 16.dp),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    text: String,
+    icon: ImageVector? = null,
+    isLoading: Boolean = false,
+) {
+    Box(
+        modifier = modifier
+            .minimumInteractiveComponentSize()
+            .graphicsLayer(shadowElevation = elevation, shape = shape, clip = false)
+            .then(if (border != null) Modifier.border(border, shape) else Modifier)
+            .background(
+                color = if (!isLoading && enabled) colors.containerColor else colors.disabledContainerColor,
+                shape = shape
+            )
+            .clip(shape)
+            .bounceClick(
+                interactionSource = interactionSource,
+                indication = null,
+                interactionColor = Color.Transparent,
+                enabled = !isLoading && enabled,
+                onClick = onClick,
+                role = Role.Button
+            ),
+        propagateMinConstraints = true
+    ) {
+        Row(
+            Modifier
+                .defaultMinSize(minHeight = 58.dp)
+                .fillMaxWidth()
+                .padding(contentPadding)
+                .animateContentSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            content = {
+                if (isLoading) {
+                    LoadingDotsIndicator()
+                } else {
+                    icon?.let {
+                        Icon(
+                            modifier = Modifier.size(20.dp),
+                            imageVector = it,
+                            contentDescription = null,
+                            tint = if (enabled) colors.contentColor else colors.disabledContentColor,
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                    }
+                    BodyLarge(
+                        text = text,
+                        color = if (enabled) colors.contentColor else colors.disabledContentColor,
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun DodamSmallFilledButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = MaterialTheme.shapes.extraSmall,
+    colors: ButtonColors = ButtonDefaults.buttonColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(0.5f),
+        disabledContainerColor = MaterialTheme.colorScheme.primary.copy(0.5f)
+    ),
+    elevation: Float = 0.0f,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = PaddingValues(horizontal = 12.dp, vertical = 12.dp),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    text: String,
+    icon: ImageVector? = null,
+    isLoading: Boolean = false,
+) {
+    Box(
+        modifier = modifier
+            .minimumInteractiveComponentSize()
+            .graphicsLayer(shadowElevation = elevation, shape = shape, clip = false)
+            .then(if (border != null) Modifier.border(border, shape) else Modifier)
+            .background(
+                color = if (!isLoading && enabled) colors.containerColor else colors.disabledContainerColor,
+                shape = shape
+            )
+            .clip(shape)
+            .bounceClick(
+                interactionSource = interactionSource,
+                indication = null,
+                interactionColor = Color.Transparent,
+                enabled = !isLoading && enabled,
+                onClick = onClick,
+                role = Role.Button
+            ),
+        propagateMinConstraints = true
+    ) {
+        Row(
+            Modifier
+                .defaultMinSize(minHeight = 36.dp)
+                .padding(contentPadding)
+                .animateContentSize(),
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            content = {
+                if (isLoading) {
+                    LoadingDotsIndicator()
+                } else {
+                    icon?.let {
+                        Icon(
+                            modifier = Modifier.size(14.dp),
+                            imageVector = it,
+                            contentDescription = null,
+                            tint = if (enabled) colors.contentColor else colors.disabledContentColor,
+                        )
+                    }
+                    BodySmall(
+                        text = text,
+                        color = if (enabled) colors.contentColor else colors.disabledContentColor,
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun DodamMediumFilledButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = MaterialTheme.shapes.small,
+    colors: ButtonColors = ButtonDefaults.buttonColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(0.5f),
+        disabledContainerColor = MaterialTheme.colorScheme.primary.copy(0.5f)
+    ),
+    elevation: Float = 0.0f,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = PaddingValues(horizontal = 20.dp, vertical = 8.dp),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    text: String,
+    icon: ImageVector? = null,
+    isLoading: Boolean = false,
+) {
+    Box(
+        modifier = modifier
+            .minimumInteractiveComponentSize()
+            .graphicsLayer(shadowElevation = elevation, shape = shape, clip = false)
+            .then(if (border != null) Modifier.border(border, shape) else Modifier)
+            .background(
+                color = if (!isLoading && enabled) colors.containerColor else colors.disabledContainerColor,
+                shape = shape
+            )
+            .clip(shape)
+            .bounceClick(
+                interactionSource = interactionSource,
+                indication = null,
+                interactionColor = Color.Transparent,
+                enabled = !isLoading && enabled,
+                onClick = onClick,
+                role = Role.Button
+            ),
+        propagateMinConstraints = true
+    ) {
+        Row(
+            Modifier
+                .defaultMinSize(minHeight = 40.dp)
+                .padding(contentPadding)
+                .animateContentSize(),
+            horizontalArrangement = Arrangement.spacedBy(6.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            content = {
+                if (isLoading) {
+                    LoadingDotsIndicator()
+                } else {
+                    icon?.let {
+                        Icon(
+                            modifier = Modifier.size(18.dp),
+                            imageVector = it,
+                            contentDescription = null,
+                            tint = if (enabled) colors.contentColor else colors.disabledContentColor,
+                        )
+                    }
+                    BodyMedium(
+                        text = text,
+                        color = if (enabled) colors.contentColor else colors.disabledContentColor,
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun DodamLargeFilledButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = MaterialTheme.shapes.medium,
+    colors: ButtonColors = ButtonDefaults.buttonColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(0.5f),
+        disabledContainerColor = MaterialTheme.colorScheme.primary.copy(0.5f)
+    ),
+    elevation: Float = 0.0f,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = PaddingValues(horizontal = 24.dp, vertical = 12.dp),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    text: String,
+    icon: ImageVector? = null,
+    isLoading: Boolean = false,
+) {
+    Box(
+        modifier = modifier
+            .minimumInteractiveComponentSize()
+            .graphicsLayer(shadowElevation = elevation, shape = shape, clip = false)
+            .then(if (border != null) Modifier.border(border, shape) else Modifier)
+            .background(
+                color = if (!isLoading && enabled) colors.containerColor else colors.disabledContainerColor,
+                shape = shape
+            )
+            .clip(shape)
+            .bounceClick(
+                interactionSource = interactionSource,
+                indication = null,
+                interactionColor = Color.Transparent,
+                enabled = !isLoading && enabled,
+                onClick = onClick,
+                role = Role.Button
+            ),
+        propagateMinConstraints = true
+    ) {
+        Row(
+            Modifier
+                .defaultMinSize(minHeight = 50.dp)
+                .padding(contentPadding)
+                .animateContentSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            content = {
+                if (isLoading) {
+                    LoadingDotsIndicator()
+                } else {
+                    icon?.let {
+                        Icon(
+                            modifier = Modifier.size(20.dp),
+                            imageVector = it,
+                            contentDescription = null,
+                            tint = if (enabled) colors.contentColor else colors.disabledContentColor,
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                    }
+                    BodyLarge(
+                        text = text,
+                        color = if (enabled) colors.contentColor else colors.disabledContentColor,
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+@Preview
+fun DodamCTAButtonPreview() {
+    DodamTheme {
+        Box(modifier = Modifier.fillMaxSize()) {
+            DodamCTAButton(onClick = { /*TODO*/ }, text = "Filled button")
+        }
+    }
+}
+
+@Composable
+@Preview
+fun DodamFilledButtonPreview() {
+    DodamTheme {
+        Column(verticalArrangement = Arrangement.spacedBy(20.dp)) {
+            DodamSmallFilledButton(
+                onClick = { /*TODO*/ },
+                text = "Filled button",
+            )
+            DodamMediumFilledButton(onClick = { /*TODO*/ }, text = "Filled button")
+            DodamLargeFilledButton(onClick = { /*TODO*/ }, text = "Filled button")
+            DodamCTAButton(onClick = { /*TODO*/ }, text = "Filled button")
+        }
+    }
+}
+
+@Composable
+@Preview
+fun DodamFilledButtonDisabledPreview() {
+    DodamTheme {
+        Column(verticalArrangement = Arrangement.spacedBy(20.dp)) {
+            DodamSmallFilledButton(
+                onClick = { /*TODO*/ },
+                text = "Filled button",
+                enabled = false
+            )
+            DodamMediumFilledButton(
+                onClick = { /*TODO*/ },
+                text = "Filled button",
+                enabled = false
+            )
+            DodamLargeFilledButton(
+                onClick = { /*TODO*/ },
+                text = "Filled button",
+                enabled = false
+            )
+            DodamCTAButton(
+                onClick = { /*TODO*/ },
+                text = "Filled button",
+                enabled = false
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun DodamFilledButtonLoadingPreview() {
+    DodamTheme {
+        Column(verticalArrangement = Arrangement.spacedBy(20.dp)) {
+            DodamSmallFilledButton(
+                onClick = { /*TODO*/ },
+                text = "Filled button",
+                isLoading = true
+            )
+            DodamMediumFilledButton(
+                onClick = { /*TODO*/ },
+                text = "Filled button",
+                isLoading = true
+            )
+            DodamLargeFilledButton(
+                onClick = { /*TODO*/ },
+                text = "Filled button",
+                isLoading = true
+            )
+            DodamCTAButton(
+                onClick = { /*TODO*/ },
+                text = "Filled button",
+                isLoading = true
+            )
+        }
+    }
+}
--- dodam-design-system/src/main/java/com/b1nd/dodam/dds/component/button/DodamIconButton.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/component/button/DodamIconButton.kt
@@ -1,15 +1,13 @@
-package com.b1nd.dodam.dds.component
+package com.b1nd.dodam.dds.component.button
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -22,8 +20,6 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.b1nd.dodam.dds.animation.bounceClick
-import com.b1nd.dodam.dds.foundation.DodamColor
-import com.b1nd.dodam.dds.foundation.DodamShape
 import com.b1nd.dodam.dds.style.HomeIcon
 import com.b1nd.dodam.dds.theme.DodamTheme
 
@@ -44,15 +40,15 @@ fun DodamIconButton(
     Box(
         modifier = modifier
             .minimumInteractiveComponentSize()
-            .size(IconButtonTokens.StateLayerSize)
-            .clip(IconButtonTokens.StateLayerShape)
+            .size(44.0.dp)
+            .clip(MaterialTheme.shapes.medium)
+            .background(color = if (enabled) colors.containerColor else colors.disabledContainerColor)
             .bounceClick(
                 onClick = onClick,
                 enabled = enabled,
                 role = Role.Button,
                 interactionSource = interactionSource
-            )
-            .background(color = if (enabled) colors.containerColor else colors.disabledContainerColor),
+            ),
         contentAlignment = Alignment.Center
     ) {
         val contentColor = if (enabled) colors.contentColor else colors.disabledContentColor
@@ -69,9 +65,3 @@ fun DodamIconButtonPreview() {
         }
     }
 }
-
-internal object IconButtonTokens {
-    val StateLayerShape = DodamShape.Large
-    val StateLayerSize = 44.0.dp
-}
-
