d38c14918e6ff2b2d1e1dca4fd9cd8b39bf8e9f4
윤석규 <greeentea.dev@gmail.com>
2024-03-21 14:49:32 +0900

    feat: Create dodam segmented button

--- app/src/main/java/com/b1nd/dodam/dds/MainActivity.kt
+++ app/src/main/java/com/b1nd/dodam/dds/MainActivity.kt
@@ -3,21 +3,16 @@ package com.b1nd.dodam.dds
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.size
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.unit.dp
-import com.b1nd.dodam.dds.component.DodamBadge
-import com.b1nd.dodam.dds.component.DodamIconButton
 import com.b1nd.dodam.dds.component.DodamNavigationBar
-import com.b1nd.dodam.dds.component.DodamNavigationItem
-import com.b1nd.dodam.dds.component.DodamTopAppBar
+import com.b1nd.dodam.dds.component.button.DodamCTAButton
+import com.b1nd.dodam.dds.component.button.DodamLargeFilledButton
+import com.b1nd.dodam.dds.component.button.DodamMediumFilledButton
+import com.b1nd.dodam.dds.component.button.DodamSmallFilledButton
 import com.b1nd.dodam.dds.component.rememberDodamNavigationItem
 import com.b1nd.dodam.dds.foundation.DodamIcons
-import com.b1nd.dodam.dds.style.DoorOpenIcon
-import com.b1nd.dodam.dds.style.ForkAndKnifeIcon
-import com.b1nd.dodam.dds.style.HomeIcon
-import com.b1nd.dodam.dds.style.MenuIcon
-import com.b1nd.dodam.dds.style.MoonPlusIcon
 import com.b1nd.dodam.dds.theme.DodamTheme
 import kotlinx.collections.immutable.persistentListOf
 
@@ -26,18 +21,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
            DodamTheme {
-               DodamNavigationBar(
-                   navigationItems = persistentListOf(
-                       rememberDodamNavigationItem("home", DodamIcons.Home),
-                       rememberDodamNavigationItem("meal", DodamIcons.ForkAndKnife),
-                       rememberDodamNavigationItem("out", DodamIcons.DoorOpen),
-                       rememberDodamNavigationItem("nightstudy", DodamIcons.MoonPlus),
-                       rememberDodamNavigationItem("menu", DodamIcons.Menu),
-                   ),
-                   onClickItem = {
-
-                   },
-               )
+               DodamTheme {
+                   Column(verticalArrangement = Arrangement.spacedBy(20.dp)) {
+                       DodamSmallFilledButton(
+                           onClick = { /*TODO*/ },
+                           text = "Filled button",
+                       )
+                       DodamMediumFilledButton(onClick = { /*TODO*/ }, text = "Filled button")
+                       DodamLargeFilledButton(onClick = { /*TODO*/ }, text = "Filled button")
+                       DodamCTAButton(onClick = { /*TODO*/ }, text = "Filled button")
+                   }
+               }
            }
         }
     }
