2fcd9a443c2961f62b93f0843884687b298f17ec
윤석규 <greeentea.dev@gmail.com>
2024-03-24 12:49:24 +0900

    feat: Create dodam textfield

--- dodam-design-system/src/main/java/com/b1nd/dodam/dds/component/DodamTextField.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/component/DodamTextField.kt
@@ -1,10 +1,223 @@
 package com.b1nd.dodam.dds.component
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.b1nd.dodam.dds.style.ExclamationMarkCircleIcon
+import com.b1nd.dodam.dds.theme.DodamTheme
 
+@ExperimentalMaterial3Api
 @Composable
 fun DodamTextField(
-
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = MaterialTheme.typography.bodyLarge,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = TextFieldDefaults.shape,
+    colors: TextFieldColors = TextFieldDefaults.colors(
+        focusedTextColor = MaterialTheme.colorScheme.onSurface,
+        unfocusedTextColor = MaterialTheme.colorScheme.onSurface,
+        disabledTextColor = MaterialTheme.colorScheme.onSurface.copy(0.5f),
+        errorTextColor = MaterialTheme.colorScheme.onSurface,
+        focusedContainerColor = Color.Transparent,
+        unfocusedContainerColor = Color.Transparent,
+        disabledContainerColor = Color.Transparent,
+        errorContainerColor = Color.Transparent,
+        cursorColor = MaterialTheme.colorScheme.primary,
+        errorCursorColor = MaterialTheme.colorScheme.error,
+        focusedIndicatorColor = MaterialTheme.colorScheme.primary,
+        unfocusedIndicatorColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledIndicatorColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(0.5f),
+        errorIndicatorColor = MaterialTheme.colorScheme.error,
+        focusedTrailingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        unfocusedTrailingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledTrailingIconColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(0.5f),
+        errorTrailingIconColor = MaterialTheme.colorScheme.error,
+        focusedLabelColor = MaterialTheme.colorScheme.primary,
+        unfocusedLabelColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(0.5f),
+        errorLabelColor = MaterialTheme.colorScheme.error,
+        focusedPlaceholderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        unfocusedPlaceholderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledPlaceholderColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(0.5f),
+        errorPlaceholderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        focusedSupportingTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        unfocusedSupportingTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledSupportingTextColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(0.5f),
+        errorSupportingTextColor = MaterialTheme.colorScheme.error,
+    )
 ) {
-    
+    val focused by interactionSource.collectIsFocusedAsState()
+
+    val textColor = textStyle.color.takeOrElse {
+        val targetValue = when {
+            !enabled -> colors.disabledTextColor
+            isError -> colors.errorTextColor
+            focused -> colors.focusedTextColor
+            else -> colors.unfocusedTextColor
+        }
+        targetValue
+    }
+
+    val supportingTextColor = when {
+        !enabled -> colors.disabledSupportingTextColor
+        isError -> colors.errorSupportingTextColor
+        focused -> colors.focusedSupportingTextColor
+        else -> colors.unfocusedSupportingTextColor
+    }
+
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+
+    Column {
+        CompositionLocalProvider(LocalTextSelectionColors provides colors.textSelectionColors) {
+            BasicTextField(
+                value = value,
+                modifier = modifier
+                    .defaultMinSize(
+                        minHeight = 56.dp
+                    ),
+                onValueChange = onValueChange,
+                enabled = enabled,
+                readOnly = readOnly,
+                textStyle = mergedTextStyle,
+                cursorBrush = SolidColor(if (isError) colors.errorCursorColor else colors.cursorColor),
+                visualTransformation = visualTransformation,
+                keyboardOptions = keyboardOptions,
+                keyboardActions = keyboardActions,
+                interactionSource = interactionSource,
+                singleLine = singleLine,
+                maxLines = maxLines,
+                minLines = minLines,
+                decorationBox = @Composable { innerTextField ->
+                    TextFieldDefaults.DecorationBox(
+                        value = value,
+                        visualTransformation = visualTransformation,
+                        innerTextField = innerTextField,
+                        placeholder = placeholder,
+                        label = label,
+                        trailingIcon = trailingIcon,
+                        suffix = suffix,
+                        shape = shape,
+                        singleLine = singleLine,
+                        enabled = enabled,
+                        isError = isError,
+                        interactionSource = interactionSource,
+                        colors = colors,
+                        contentPadding = PaddingValues(vertical = 8.dp),
+                    )
+                }
+            )
+        }
+
+        supportingText?.let {
+            Spacer(modifier = Modifier.height(4.dp))
+            CompositionLocalProvider(
+                LocalTextStyle provides MaterialTheme.typography.labelLarge,
+                LocalContentColor provides supportingTextColor,
+                content = it
+            )
+        }
+    }
+}
+
+
+@ExperimentalMaterial3Api
+@Composable
+@Preview(showBackground = true)
+fun DodamTextFieldPreview() {
+    var text by remember { mutableStateOf("") }
+
+    DodamTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp), verticalArrangement = Arrangement.Center
+        ) {
+            DodamTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = text,
+                onValueChange = { text = it },
+                placeholder = { Text(text = "Place holder") },
+                label = { Text(text = "Label text") },
+                supportingText = {
+                    Text(text = "Supporting Text")
+                }
+            )
+
+            DodamTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = text,
+                onValueChange = { text = it },
+                placeholder = { Text(text = "Place holder") },
+                label = { Text(text = "Label text") },
+                enabled = false,
+                supportingText = {
+                    Text(text = "Supporting Text")
+                }
+            )
+            DodamTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = text,
+                onValueChange = { text = it },
+                placeholder = { Text(text = "Place holder") },
+                label = { Text(text = "Label text") },
+                isError = true,
+                supportingText = {
+                    Text(text = "Supporting Text")
+                },
+                trailingIcon = {
+                    ExclamationMarkCircleIcon()
+                }
+            )
+        }
+    }
 }
