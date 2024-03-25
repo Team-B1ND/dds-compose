5cddc5f2a1dcedda57887c5d5a66598880a23286
윤석규 <greeentea.dev@gmail.com>
2024-03-25 15:58:52 +0900

    delete: Remove window insets

--- dodam-design-system/src/main/java/com/b1nd/dodam/dds/theme/DodamTheme.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/theme/DodamTheme.kt
@@ -1,14 +1,8 @@
 package com.b1nd.dodam.dds.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import com.b1nd.dodam.dds.style.DodamDarkColorScheme
 import com.b1nd.dodam.dds.style.DodamLightColorScheme
 import com.b1nd.dodam.dds.style.DodamShapes
@@ -19,18 +13,6 @@ fun DodamTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
-            windowInsetsController.apply {
-                hide(WindowInsetsCompat.Type.systemBars())
-                systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        }
-    }
-
     MaterialTheme(
         colorScheme = if (darkTheme) DodamDarkColorScheme else DodamLightColorScheme,
         typography = DodamTypography,
