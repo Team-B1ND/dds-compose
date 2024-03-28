51b2df680b78e27ef5297f36e6ddfde4f984c3aa
윤석규 <seokgyu1124@icloud.com>
2024-03-28 22:58:50 +0900

    refactor: Modify not to click when selected

--- app/src/main/java/com/b1nd/dodam/dds/MainActivity.kt
+++ app/src/main/java/com/b1nd/dodam/dds/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,10 +19,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.b1nd.dodam.dds.component.DodamDialog
+import com.b1nd.dodam.dds.component.DodamNavigationBar
 import com.b1nd.dodam.dds.component.button.DodamCTAButton
 import com.b1nd.dodam.dds.component.button.DodamLargeFilledButton
 import com.b1nd.dodam.dds.component.button.DodamTextButton
+import com.b1nd.dodam.dds.component.rememberDodamNavigationItem
+import com.b1nd.dodam.dds.foundation.DodamIcons
 import com.b1nd.dodam.dds.theme.DodamTheme
+import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.delay
 
 @ExperimentalMaterial3Api
@@ -29,19 +34,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var enabled by remember { mutableStateOf(false) }
-            
-            LaunchedEffect(Unit) {
-                delay(3000)
-                enabled = true
-            }
+            val list = persistentListOf(
+                rememberDodamNavigationItem("home", DodamIcons.Home),
+                rememberDodamNavigationItem("meal", DodamIcons.ForkAndKnife),
+                rememberDodamNavigationItem("out", DodamIcons.DoorOpen),
+                rememberDodamNavigationItem("nightstudy", DodamIcons.MoonPlus),
+                rememberDodamNavigationItem("menu", DodamIcons.Menu),
+            )
 
             DodamTheme {
-                DodamCTAButton(
-                    onClick = { /*TODO*/ },
-                    enabled = enabled
-                ) {
-                    Text(text = "TEST")
+                Box(Modifier.fillMaxSize()) {
+                    DodamNavigationBar(navigationItems = list) {
+
+                    }
                 }
             }
         }
