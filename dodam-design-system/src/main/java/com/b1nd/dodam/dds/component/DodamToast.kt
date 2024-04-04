2fbb96b0e913706e9eab51d8095a8dbd788158b9
윤석규 <seokgyu1124@icloud.com>
2024-04-04 21:08:01 +0900

    feat: Add success, warning, error toast

--- dodam-design-system/src/main/java/com/b1nd/dodam/dds/component/DodamToast.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/dds/component/DodamToast.kt
@@ -1,6 +1,7 @@
 package com.b1nd.dodam.dds.component
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
@@ -17,9 +18,12 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.b1nd.dodam.dds.R
 import com.b1nd.dodam.dds.foundation.DodamColor
 import com.b1nd.dodam.dds.style.CheckmarkCircleFilledIcon
 import com.b1nd.dodam.dds.style.CheckmarkCircleIcon
@@ -50,22 +54,75 @@ fun DodamToast(
 }
 
 @Composable
+fun DodamSuccessToast(
+    text: String,
+    textColor: Color = DodamColor.White,
+    containerColor: Color = if (isSystemInDarkTheme()) DodamColor.Gray750 else DodamColor.Gray500,
+) {
+    Row(
+        modifier = Modifier
+            .background(containerColor, CircleShape)
+            .padding(horizontal = 16.dp, vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Image(
+            modifier = Modifier.size(20.dp),
+            imageVector = ImageVector.vectorResource(R.drawable.ic_colored_checkmark_circle_filled),
+            contentDescription = null
+        )
+        LabelLarge(text = text, fontWeight = FontWeight.SemiBold, color = textColor)
+    }
+}
+
+@Composable
+fun DodamErrorToast(
+    text: String,
+    textColor: Color = DodamColor.White,
+    containerColor: Color = if (isSystemInDarkTheme()) DodamColor.Gray750 else DodamColor.Gray500,
+) {
+    Row(
+        modifier = Modifier
+            .background(containerColor, CircleShape)
+            .padding(horizontal = 16.dp, vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Image(
+            modifier = Modifier.size(20.dp),
+            imageVector = ImageVector.vectorResource(R.drawable.ic_colored_xmark_circle),
+            contentDescription = null
+        )
+        LabelLarge(text = text, fontWeight = FontWeight.SemiBold, color = textColor)
+    }
+}
+
+@Composable
+fun DodamWarningToast(
+    text: String,
+    textColor: Color = DodamColor.White,
+    containerColor: Color = if (isSystemInDarkTheme()) DodamColor.Gray750 else DodamColor.Gray500,
+) {
+    Row(
+        modifier = Modifier
+            .background(containerColor, CircleShape)
+            .padding(horizontal = 16.dp, vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Image(
+            modifier = Modifier.size(20.dp),
+            imageVector = ImageVector.vectorResource(R.drawable.ic_colored_exclamationmark_circle),
+            contentDescription = null
+        )
+        LabelLarge(text = text, fontWeight = FontWeight.SemiBold, color = textColor)
+    }
+}
+
+@Composable
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 private fun DodamToastPreview() {
     DodamTheme {
-        DodamToast(
-            text = "외출 신청을 성공했어요",
-            trailingIcon = { CheckmarkCircleFilledIcon(
-                modifier = Modifier
-                    .size(20.dp)
-                    .drawBehind {
-                        drawRoundRect(
-                            color = DodamColor.White,
-                            topLeft = Offset(12f, 12f),
-                            size = Size(30f, 30f)
-                        )
-                    }
-            ) }
-        )
+        DodamSuccessToast(text = "외출 신청을 성공했어요")
     }
 }
