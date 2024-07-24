433fd3c52d145a693820c7ed08079b637d4fddae
윤석규 <greeentea.dev@gmail.com>
2024-07-24 14:55:56 +0900

    refactor: Modify foundations

--- dodam-design-system/src/main/java/com/b1nd/dodam/designsystem/foundation/DodamShapes.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/designsystem/foundation/DodamShapes.kt
@@ -7,14 +7,11 @@ import com.b1nd.dodam.designsystem.tokens.ShapeTokens
 
 @Immutable
 data class DodamShapes(
-    val xxSmall: CornerBasedShape = ShapeTokens.XXSmall,
-    val xSmall: CornerBasedShape = ShapeTokens.XSmall,
+    val extraSmall: CornerBasedShape = ShapeTokens.ExtraSmall,
     val small: CornerBasedShape = ShapeTokens.Small,
     val medium: CornerBasedShape = ShapeTokens.Medium,
     val large: CornerBasedShape = ShapeTokens.Large,
-    val xLarge: CornerBasedShape = ShapeTokens.XLarge,
-    val xxLarge: CornerBasedShape = ShapeTokens.XXLarge,
-    val xxxLarge: CornerBasedShape = ShapeTokens.XXXlarge,
+    val extraLarge: CornerBasedShape = ShapeTokens.ExtraLarge,
 )
 
 internal val LocalDodamShapes = staticCompositionLocalOf { DodamShapes() }
--- dodam-design-system/src/main/java/com/b1nd/dodam/designsystem/tokens/TypographyTokens.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/designsystem/tokens/TypographyTokens.kt
@@ -7,90 +7,226 @@ import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 
 internal object TypographyTokens {
-    val Headline1 = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightBold,
-        fontSize = 28.sp,
-        lineHeight = 1.3.em,
-    )
-    val Headline2 = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightBold,
-        fontSize = 24.sp,
-        lineHeight = 1.3.em,
-    )
-    val Headline3 = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightBold,
-        fontSize = 18.sp,
-        lineHeight = 1.3.em,
-    )
-    val Title1 = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightBold,
-        fontSize = 20.sp,
-        lineHeight = 1.3.em,
-    )
-    val Title2 = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightSemiBold,
-        fontSize = 18.sp,
-        lineHeight = 1.3.em,
-    )
-    val Title3 = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightSemiBold,
-        fontSize = 16.sp,
-        lineHeight = 1.3.em,
-    )
-    val Body1Strong = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightSemiBold,
-        fontSize = 16.sp,
-        lineHeight = 1.3.em,
-    )
-    val Body1Normal = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightMedium,
-        fontSize = 16.sp,
-        lineHeight = 1.3.em,
-    )
-    val Body2Strong = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightSemiBold,
-        fontSize = 15.sp,
-        lineHeight = 1.3.em,
-    )
-    val Body2Normal = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightMedium,
-        fontSize = 15.sp,
-        lineHeight = 1.3.em,
-    )
-    val Body3Strong = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightSemiBold,
-        fontSize = 13.sp,
-        lineHeight = 1.3.em,
-    )
-    val Body3Normal = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightMedium,
-        fontSize = 13.sp,
-        lineHeight = 1.3.em,
-    )
-    val Label1 = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightRegular,
-        fontSize = 13.sp,
-        lineHeight = 1.3.em,
-    )
-    val Label2 = DefaultTextStyle.copy(
-        fontFamily = TypefaceTokens.Pretendard,
-        fontWeight = TypefaceTokens.WeightRegular,
-        fontSize = 12.sp,
-        lineHeight = 1.3.em,
-    )
+    object Title1 {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightExtraBold,
+            fontSize = 36.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 36.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 36.sp,
+            lineHeight = 1.3.em,
+        )
+    }
+    object Title2 {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightExtraBold,
+            fontSize = 28.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 28.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 28.sp,
+            lineHeight = 1.3.em,
+        )
+    }
+    object Title3 {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightExtraBold,
+            fontSize = 24.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 24.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 24.sp,
+            lineHeight = 1.3.em,
+        )
+    }
+    object Heading1 {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightExtraBold,
+            fontSize = 22.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 22.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 22.sp,
+            lineHeight = 1.3.em,
+        )
+    }
+    object Heading2 {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightExtraBold,
+            fontSize = 20.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 20.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 20.sp,
+            lineHeight = 1.3.em,
+        )
+    }
+    object Headline {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightBold,
+            fontSize = 18.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 18.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 18.sp,
+            lineHeight = 1.3.em,
+        )
+    }
+    object Body1 {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightSemiBold,
+            fontSize = 16.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 16.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 16.sp,
+            lineHeight = 1.3.em,
+        )
+    }
+    object Body2 {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightSemiBold,
+            fontSize = 15.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 15.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 15.sp,
+            lineHeight = 1.3.em,
+        )
+    }
+    object Label {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightSemiBold,
+            fontSize = 14.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 14.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 14.sp,
+            lineHeight = 1.3.em,
+        )
+    }
+    object Caption1 {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightSemiBold,
+            fontSize = 13.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 13.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 13.sp,
+            lineHeight = 1.3.em,
+        )
+    }
+    object Caption2 {
+        val Bold = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightSemiBold,
+            fontSize = 12.sp,
+            lineHeight = 1.3.em,
+        )
+        val Medium = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightMedium,
+            fontSize = 12.sp,
+            lineHeight = 1.3.em,
+        )
+        val Regular = DefaultTextStyle.copy(
+            fontFamily = TypefaceTokens.Pretendard,
+            fontWeight = TypefaceTokens.WeightRegular,
+            fontSize = 12.sp,
+            lineHeight = 1.3.em,
+        )
+    }
 }
 
 
