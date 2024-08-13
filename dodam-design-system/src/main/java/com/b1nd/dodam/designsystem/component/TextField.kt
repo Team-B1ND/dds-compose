bbab8aee87dfc59a18f58af2af3700f338c96a3c
8954sood <8954sood@naver.com>
2024-08-13 13:48:35 +0900

    feat: Create FilledTextField Component

--- dodam-design-system/src/main/java/com/b1nd/dodam/designsystem/component/TextField.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/designsystem/component/TextField.kt
@@ -1,7 +1,6 @@
 package com.b1nd.dodam.designsystem.component
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.util.Log
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -24,33 +23,38 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.min
 import androidx.compose.ui.unit.toSize
 import com.b1nd.dodam.designsystem.DodamTheme
 import com.b1nd.dodam.designsystem.R
@@ -73,6 +77,14 @@ fun DodamTextField(
     supportText: String = "",
     isError: Boolean = false,
     enabled: Boolean = true,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    cursorBrush: Brush = SolidColor(Color.Black),
     onClickRemoveRequest: () -> Unit = {},
 ) {
     val focusRequester by remember { mutableStateOf(FocusRequester()) }
@@ -100,8 +112,8 @@ fun DodamTextField(
         label = "Text Transition"
     )
 
-    val labelTextStyle = DodamTextFieldDefaults.labelTextStyle
-    val labelSmallTextStyle = DodamTextFieldDefaults.labelSmallTextStyle
+    val labelTextStyle = DodamTextFieldDefaults.LabelTextStyle
+    val labelSmallTextStyle = DodamTextFieldDefaults.LabelSmallTextStyle
     val labelAnimation = remember { Animatable(0f) }
     val labelAnimTextStyle by remember(labelAnimation.value) {
         derivedStateOf {
@@ -118,8 +130,8 @@ fun DodamTextField(
 
     val animateLabelColor by animateColorAsState(
         targetValue = when(currentInputType) {
-            is InputType.Focus -> DodamTextFieldDefaults.focusColor
-            is InputType.Error -> DodamTextFieldDefaults.errorColor
+            is InputType.Focus -> DodamTextFieldDefaults.FocusColor
+            is InputType.Error -> DodamTextFieldDefaults.ErrorColor
             else -> DodamTextFieldDefaults.LabelColor
         },
         label = "",
@@ -127,8 +139,8 @@ fun DodamTextField(
 
     val animateStrokeColor by animateColorAsState(
         targetValue = when(currentInputType) {
-            is InputType.Focus -> DodamTextFieldDefaults.focusColor
-            is InputType.Error -> DodamTextFieldDefaults.errorColor
+            is InputType.Focus -> DodamTextFieldDefaults.FocusColor
+            is InputType.Error -> DodamTextFieldDefaults.ErrorColor
             else -> DodamTextFieldDefaults.StrokeColor
         },
         label = "",
@@ -136,7 +148,7 @@ fun DodamTextField(
 
     val animateSupportTextColor by animateColorAsState(
         targetValue = when(currentInputType) {
-            is InputType.Error -> DodamTextFieldDefaults.errorColor
+            is InputType.Error -> DodamTextFieldDefaults.ErrorColor
             else -> DodamTextFieldDefaults.SupportTextColor
         },
         label = "",
@@ -163,9 +175,17 @@ fun DodamTextField(
                 },
             value = value,
             onValueChange = onValueChange,
-            textStyle = DodamTextFieldDefaults.textStyle.copy(
-                color = DodamTextFieldDefaults.textColor
+            textStyle = DodamTextFieldDefaults.TextStyle.copy(
+                color = DodamTextFieldDefaults.TextColor
             ),
+            keyboardActions = keyboardActions,
+            keyboardOptions = keyboardOptions,
+            singleLine = singleLine,
+            maxLines = maxLines,
+            minLines = minLines,
+            visualTransformation = visualTransformation,
+            interactionSource = interactionSource,
+            cursorBrush = cursorBrush,
             enabled = enabled
         ) { innerTextField ->
             Row(
@@ -217,7 +237,7 @@ fun DodamTextField(
                             Image(
                                 modifier = Modifier.size(24.dp),
                                 imageVector = rememberIcColoredExclamationMarkCircle(
-                                    backgroundColor = DodamTextFieldDefaults.errorColor
+                                    backgroundColor = DodamTextFieldDefaults.ErrorColor
                                 ),
                                 contentDescription = null
                             )
@@ -255,23 +275,223 @@ fun DodamTextField(
     }
 }
 
+@Composable
+fun DodamFilledTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String = "",
+    supportText: String = "",
+    isError: Boolean = false,
+    enabled: Boolean = true,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    cursorBrush: Brush = SolidColor(Color.Black),
+    onClickRemoveRequest: () -> Unit = {},
+) {
+    val focusRequester by remember { mutableStateOf(FocusRequester()) }
+    var isFocus by remember { mutableStateOf(false) }
+    val density = LocalDensity.current
+
+    val currentInputType by remember {
+        derivedStateOf {
+            focusStateAsInputType(
+                isFocused = isFocus,
+                currentValue = value,
+                isError = isError
+            )
+        }
+    }
+
+    val animateLabelColor by animateColorAsState(
+        targetValue = when(currentInputType) {
+            is InputType.Focus -> DodamTextFieldDefaults.FocusColor
+            is InputType.Error -> DodamTextFieldDefaults.ErrorColor
+            else -> DodamTextFieldDefaults.LabelColor
+        },
+        label = "",
+    )
+
+    val animateStrokeColor by animateColorAsState(
+        targetValue = when(currentInputType) {
+            is InputType.Focus -> DodamTextFieldDefaults.FocusColor
+            is InputType.Error -> DodamTextFieldDefaults.ErrorColor
+            else -> DodamTextFieldDefaults.StrokeColor
+        },
+        label = "",
+    )
+
+    val animateSupportTextColor by animateColorAsState(
+        targetValue = when(currentInputType) {
+            is InputType.Error -> DodamTextFieldDefaults.ErrorColor
+            else -> DodamTextFieldDefaults.SupportTextColor
+        },
+        label = "",
+    )
+
+    val animateBackgroundColor by animateColorAsState(
+        targetValue = when(currentInputType) {
+            is InputType.Focus -> DodamTextFieldDefaults.BackgroundFocusColor
+            is InputType.Error -> DodamTextFieldDefaults.BackgroundErrorColor
+            else -> DodamTextFieldDefaults.BackgroundNormal
+        },
+        label = "",
+    )
+
+    var boxHeight by remember { mutableStateOf(56.dp) }
+
+    Box(
+        modifier = modifier
+            .heightIn(min = 56.dp)
+            .alpha(if (enabled) 1f else 0.65f)
+            .onGloballyPositioned { coordinates ->
+                boxHeight = density.run { coordinates.size.toSize().height.toDp() }
+            }
+    ) {
+        Column {
+            Text(
+                modifier = Modifier,
+                text = label,
+                style = DodamTextFieldDefaults.LabelSmallTextStyle,
+                color = animateLabelColor
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            BasicTextField(
+                modifier = Modifier
+                    .focusRequester(focusRequester)
+                    .onFocusChanged {
+                        isFocus = it.isFocused
+                    },
+                value = value,
+                onValueChange = onValueChange,
+                textStyle = DodamTextFieldDefaults.TextStyle.copy(
+                    color = DodamTextFieldDefaults.TextColor
+                ),
+                keyboardActions = keyboardActions,
+                keyboardOptions = keyboardOptions,
+                singleLine = singleLine,
+                maxLines = maxLines,
+                minLines = minLines,
+                visualTransformation = visualTransformation,
+                interactionSource = interactionSource,
+                cursorBrush = cursorBrush,
+                enabled = enabled
+            ) { innerTextField ->
+                Box(
+                    modifier = Modifier
+                        .border(
+                            width = 1.dp,
+                            color = animateStrokeColor,
+                            shape = DodamTheme.shapes.medium
+                        )
+                        .drawBehind {
+                            drawRoundRect(
+                                color = animateBackgroundColor,
+                                cornerRadius = CornerRadius(12.dp.toPx())
+                            )
+                        }
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .heightIn(
+                                min = 48.dp
+                            )
+                            .fillMaxWidth()
+                            .padding(vertical = 4.dp)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(
+                                    start = 16.dp,
+                                    top = (10.5).dp,
+                                    bottom = (10.5).dp
+                                )
+                                .weight(1f)
+                        ) {
+                            innerTextField()
+                        }
+
+                        if (currentInputType !is InputType.Default && value.isNotEmpty()) {
+
+                            Box(
+                                modifier = Modifier
+                                    .align(Alignment.CenterVertically)
+                                    .size(32.dp)
+                                    .padding(
+                                        end = 12.dp
+                                    )
+                                    .clickable(
+                                        onClick = onClickRemoveRequest,
+                                        interactionSource = remember { MutableInteractionSource() },
+                                        indication = rememberBounceIndication()
+                                    ),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                if (isError) {
+                                    Image(
+                                        modifier = Modifier.size(24.dp),
+                                        imageVector = rememberIcColoredExclamationMarkCircle(
+                                            backgroundColor = DodamTextFieldDefaults.ErrorColor
+                                        ),
+                                        contentDescription = null
+                                    )
+                                } else {
+                                    Image(
+                                        modifier = Modifier.size(24.dp),
+                                        painter = painterResource(id = R.drawable.ic_xmark_circle),
+                                        contentDescription = null,
+                                        colorFilter = ColorFilter.tint(DodamTextFieldDefaults.LabelColor)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (supportText.isNotEmpty()) {
+            Text(
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .offset(
+                        y = boxHeight + 4.dp
+                    ),
+                text = supportText,
+                color = animateSupportTextColor,
+                style = DodamTextFieldDefaults.SupportTextStyle
+            )
+        }
+    }
+}
+
 
 @Stable
 object DodamTextFieldDefaults {
 
-    val labelTextStyle @Composable get() = DodamTheme.typography.headlineMedium
-    val labelSmallTextStyle @Composable get() = DodamTheme.typography.labelMedium
+    val LabelTextStyle @Composable get() = DodamTheme.typography.headlineMedium
+    val LabelSmallTextStyle @Composable get() = DodamTheme.typography.labelMedium
     val LabelColor @Composable get() = DodamTheme.colors.labelAlternative
 
-    val textStyle @Composable get() = DodamTheme.typography.headlineMedium
-    val textColor @Composable get() = DodamTheme.colors.labelStrong
-    val StrokeColor @Composable get() = DodamTheme.colors.labelNormal
+    val TextStyle @Composable get() = DodamTheme.typography.headlineMedium
+    val TextColor @Composable get() = DodamTheme.colors.labelStrong
+    val StrokeColor @Composable get() = DodamTheme.colors.lineNormal
+
 
     val SupportTextStyle @Composable get() = DodamTheme.typography.labelMedium
     val SupportTextColor @Composable get() = DodamTheme.colors.labelAlternative
 
-    val errorColor @Composable get() = DodamTheme.colors.statusNegative
-    val focusColor @Composable get() = DodamTheme.colors.primaryNormal
+    val BackgroundNormal @Composable get() = DodamTheme.colors.backgroundNormal
+    val BackgroundFocusColor @Composable get() = Color(0x03008BFF)
+    val BackgroundErrorColor @Composable get() = Color(0x03E52222)
+
+    val ErrorColor @Composable get() = DodamTheme.colors.statusNegative
+    val FocusColor @Composable get() = DodamTheme.colors.primaryNormal
 }
 
 private fun focusStateAsInputType(
@@ -301,6 +521,7 @@ private fun DodamTextFieldPreview() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .background(DodamTheme.colors.backgroundNormal)
                 .pointerInput(Unit) {
                     detectTapGestures(onTap = {
                         focusManager.clearFocus()
@@ -348,6 +569,42 @@ private fun DodamTextFieldPreview() {
             )
             Spacer(modifier = Modifier.height(24.dp))
             DodamTextField(
+                value = value,
+                onValueChange = {
+                    value = it
+                },
+                label = "Label text",
+                isError = true,
+                enabled = true,
+                supportText = "Supporting text",
+                onClickRemoveRequest = {}
+            )
+
+            DodamFilledTextField(
+                value = value,
+                onValueChange = {
+                    value = it
+                },
+                label = "Label text",
+                isError = false,
+                enabled = true,
+                supportText = "Supporting text",
+                onClickRemoveRequest = {}
+            )
+
+            DodamFilledTextField(
+                value = value,
+                onValueChange = {
+                    value = it
+                },
+                label = "Label text",
+                isError = false,
+                enabled = false,
+                supportText = "Supporting text",
+                onClickRemoveRequest = {}
+            )
+
+            DodamFilledTextField(
                 value = value,
                 onValueChange = {
                     value = it
