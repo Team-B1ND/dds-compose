071f81ed8f4b3184f134f684c5c7c45b1a0a9b74
8954sood <8954sood@naver.com>
2024-08-13 15:08:08 +0900

    feat: Modal Bottom Sheet Add Navigation Bar Padding

--- dodam-design-system/src/main/java/com/b1nd/dodam/designsystem/component/ModalBottomSheet.kt
+++ dodam-design-system/src/main/java/com/b1nd/dodam/designsystem/component/ModalBottomSheet.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.BottomSheetScaffold
@@ -67,6 +68,7 @@ fun DodamModalBottomSheet(
                 title()
                 Spacer(modifier = Modifier.height(4.dp))
                 content()
+                Spacer(modifier = Modifier.navigationBarsPadding())
             }
         }
     )
