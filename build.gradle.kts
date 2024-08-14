fb0f12c04dbbb80de914c6a211994b58545dce2a
8954sood <8954sood@naver.com>
2024-08-14 21:58:31 +0900

    update: Update version
    Version 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.kotlin.android).apply(false)
     alias(libs.plugins.compose.compiler).apply(false)
-//    alias(libs.plugins.android.maven).apply(false)
+    alias(libs.plugins.multiplatform.maven).apply(false)
     alias(libs.plugins.kotlin.multiplatform).apply(false)
     alias(libs.plugins.jetbrains.compose).apply(false)
 }
