9d73221f6b96c36d6264f04e5ce4b6b50ab79b32
8954sood <8954sood@naver.com>
2024-08-28 21:21:33 +0900

    fix: Background Neutral Color Reversed

--- dodam-design-system/src/commonMain/kotlin/com/b1nd/dodam/designsystem/tokens/ColorDarkTokens.kt
+++ dodam-design-system/src/commonMain/kotlin/com/b1nd/dodam/designsystem/tokens/ColorDarkTokens.kt
@@ -18,7 +18,7 @@ internal object ColorDarkTokens {
     object Background {
         val Normal = PaletteTokens.Neutral15
         val Alternative = PaletteTokens.Neutral5
-        val Neutral = PaletteTokens.Neutral99
+        val Neutral = PaletteTokens.Neutral10
     }
 
     object Line {
