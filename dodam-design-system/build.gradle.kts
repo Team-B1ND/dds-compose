652c0282bda5fa527e35d534774c8cdbca602302
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-09-09 20:40:06 +0900

    chore: Version Up

--- dodam-design-system/build.gradle.kts
+++ dodam-design-system/build.gradle.kts
@@ -80,7 +80,7 @@ mavenPublishing {
     coordinates(
         groupId = "com.b1nd.dodam",
         artifactId = "dodam-design-system",
-        version = "1.0.4"
+        version = "1.0.5"
     )
 
     pom {
