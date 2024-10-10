27051cdc866644c4c8cf46afb66da65aba646aa1
8954sood <8954sood@naver.com>
2024-10-10 09:38:27 +0900

    docs: Version Up to 1.0.9

--- dodam-design-system/build.gradle.kts
+++ dodam-design-system/build.gradle.kts
@@ -80,7 +80,7 @@ mavenPublishing {
     coordinates(
         groupId = "com.b1nd.dodam",
         artifactId = "dodam-design-system",
-        version = "1.0.8"
+        version = "1.0.9"
     )
 
     pom {
