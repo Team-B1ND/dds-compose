5659a8ec826cdb20d403cb19f812e2fcb93b813d
8954sood <8954sood@naver.com>
2024-12-02 16:00:07 +0900

    fix: Remove Button Vertical Position

--- app/src/main/java/com/b1nd/dodam/designsystem/MainActivity.kt
+++ app/src/main/java/com/b1nd/dodam/designsystem/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,13 +12,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.b1nd.dodam.designsystem.component.AvatarSize
 import com.b1nd.dodam.designsystem.component.DodamAvatar
 import com.b1nd.dodam.designsystem.component.DodamAvatarBorder
 import com.b1nd.dodam.designsystem.component.DodamCircularProgressIndicator
+import com.b1nd.dodam.designsystem.component.DodamFilledTextField
 import com.b1nd.dodam.designsystem.component.DodamLinerProgressIndicator
+import com.b1nd.dodam.designsystem.component.DodamTextField
 import com.b1nd.dodam.designsystem.foundation.DodamIcons
 
 @ExperimentalMaterial3Api
@@ -25,18 +33,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var value by remember { mutableStateOf("testValueqwewqjeoqw\nqwe\n\n\n\n\nqeqwe") }
+            var textFieldValue by remember { mutableStateOf(TextFieldValue("qwe\nqwe\nqwe\nqwe\nqwe\nqwe\nqwe\nqwe")) }
             DodamTheme {
-                Column {
-                    Icon(
-                        modifier = Modifier.size(40.dp),
-                        imageVector = DodamIcons.File.value,
-                        contentDescription = null
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    DodamTextField(
+                        value = value,
+                        onValueChange = {
+                            value = it
+                        }
                     )
-                    Icon(
-                        modifier = Modifier.size(40.dp),
-                        imageVector = DodamIcons.Photo.value,
-                        contentDescription = null
+
+                    DodamFilledTextField(
+                        value = value,
+                        onValueChange = {
+                            value = it
+                        }
                     )
+
+                    DodamTextField(
+                        value = textFieldValue,
+                        onValueChange = {
+                            textFieldValue = it
+                        }
+                    )
+                    DodamFilledTextField(
+                        value = textFieldValue,
+                        onValueChange = {
+                            textFieldValue = it
+                        }
+                    )
+
+
                 }
             }
         }
